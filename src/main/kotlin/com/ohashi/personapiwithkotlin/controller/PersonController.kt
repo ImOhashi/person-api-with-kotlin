5c37e799a86e702df36dfa982320d09036e9f3e4
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-17 00:49:17 -0300

    feat: add controller to find by id

--- src/main/kotlin/com/ohashi/personapiwithkotlin/controller/PersonController.kt
+++ src/main/kotlin/com/ohashi/personapiwithkotlin/controller/PersonController.kt
@@ -1,7 +1,19 @@
 package com.ohashi.personapiwithkotlin.controller
 
+import com.ohashi.personapiwithkotlin.model.Person
+import com.ohashi.personapiwithkotlin.service.PersonService
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PersonController {
+@RequestMapping("/person")
+class PersonController(
+    private val service: PersonService
+) {
+
+    @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findById(@PathVariable("id") id: Long) = service.findById(id)
 }
