749e8663246e2c32f0060cde3c2eec3ae2251bc3
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-17 00:41:35 -0300

    feat: add find person by id with mocked person

--- src/main/kotlin/com/ohashi/personapiwithkotlin/service/PersonService.kt
+++ src/main/kotlin/com/ohashi/personapiwithkotlin/service/PersonService.kt
@@ -1,7 +1,30 @@
 package com.ohashi.personapiwithkotlin.service
 
+import com.ohashi.personapiwithkotlin.model.Person
 import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicLong
+import java.util.logging.Logger
 
 @Service
 class PersonService {
+
+    private val counter: AtomicLong = AtomicLong()
+
+    fun findById(id: Long): Person {
+        logger.info { "Find a person with id=$id" }
+
+        val person = Person()
+
+        person.id = counter.incrementAndGet()
+        person.firstName = "Leonardo"
+        person.lastName = "Ohashi"
+        person.address = "São Paulo"
+        person.gender = "Male"
+
+        return person
+    }
+
+    companion object {
+        val logger = Logger.getLogger(PersonService::class.java.name)
+    }
 }
